1a168f5b51357c291d861cae19ca83210bcc1b41
Armatroll-BenjaDarte <91312951+Ben-Drt@users.noreply.github.com>
2021-10-07 15:48:02 +0200

    Algo: Catégorie Age

---
+++ Exercice.cs
@@ -0,0 +1,37 @@
+using System;
+
+/// <summary>
+/// </summary>
+public class Exercice
+{
+	public static void Main(string[] args)
+	{
+
+		Catégorie Sportive;
+		if (age<=5)
+		{
+			??? Catégorie = aucune catégorie Sportive;
+		}
+		else if (age<=7)
+			{
+				Catégorie = catégorie Poussin;
+			}
+			else if (age<=9)
+				{
+					Catégorie = catégorie Pupille;
+				}
+				else if (age<=11)
+					{
+						Catégorie = catégorie Minime;
+					}
+					else if (age<=14)
+						{
+							Catégorie = catégorie Cadet;
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
