66818c1e3a8417c9b9f899dd3d2d22b48b9234f9
Armatroll-BenjaDarte <91312951+Ben-Drt@users.noreply.github.com>
2021-10-07 16:12:56 +0200

    Table de 5

---
+++ Table_de_5.cs
@@ -0,0 +1,22 @@
+using System;
+
+/// <summary>
+/// </summary>
+public class Table_de_5
+{
+
+	/// <param name="args"> array of command line arguments </param>
+	public static void Main(string[] args)
+	{
+		int nombre;
+
+		nombre = 0;
+		for (int n = ?; n <= 1 to 100; n += (1))
+		{
+			??? total = n*5;
+			nombre = nombre+","+total;
+		}
+		Console.WriteLine(nombre);
+	}
+
+}
